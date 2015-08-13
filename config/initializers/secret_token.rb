5431aab1c3e85aa40602c2dfe38c81791176953f
= <=>
2015-08-13 10:03:12 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polls::Application.config.secret_key_base = '202b5cf73197b8d9f20b88c8397e6fe163aee12cb7b73b3d41565072df5ea332759696753dc542c13e87a9e94c07963b9d1a72029424e87ea2e27ca2ff5c3af9'
