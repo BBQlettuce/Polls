f375212377a6edea1d598f1f4bf5fc242f087c24
= <=>
2015-08-13 15:40:13 -0400

    correct completed_polls, add uncompleted_polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,37 +16,45 @@ class User < ActiveRecord::Base
     Poll
       .joins(questions: :answer_choices)
       .joins('LEFT OUTER JOIN responses ON responses.answer_choice_id = answer_choices.id' )
-      .where('responses.user_id = ?', id)
+      .where('responses.user_id = ? or responses.user_id IS NULL', id)
       .group("polls.id")
       .having("COUNT(DISTINCT questions.id) = COUNT(responses.id)")
       .select("polls.*")
   end
 
+  def uncompleted_polls
+    Poll
+      .joins(questions: :answer_choices)
+      .joins('LEFT OUTER JOIN responses ON responses.answer_choice_id = answer_choices.id' )
+      .where('responses.user_id = ? or responses.user_id IS NULL', id)
+      .group("polls.id")
+      .having("COUNT(DISTINCT questions.id) != COUNT(responses.id)")
+      .select("polls.*")
+  end
+
 end
       # .joins("?", responses)
-# +
-#
+# # +
+# # # #
 # SELECT
-#   polls.*
+#   polls.* -- polls.id p_id, questions.id q_id, answer_choices.id a_id, responses.id r_id, responses.user_id u_id
 # FROM
 #   polls
-# JOIN
+# LEFT OUTER JOIN
 #   questions ON polls.id = questions.poll_id
-# JOIN
+# LEFT OUTER JOIN
 #   answer_choices ON answer_choices.question_id = questions.id
 # LEFT OUTER JOIN
-#   (SELECT
-#     *
-#   FROM
-#     responses
-#   WHERE
-#     responses.user_id = 2) AS user_responses
-#   ON user_responses.answer_choice_id = answer_choices.id
+#   responses ON responses.answer_choice_id = answer_choices.id
+# WHERE
+#   responses.user_id = 2 OR responses.user_id IS NULL
 # GROUP BY
 #   polls.id
 # HAVING
-#   COUNT(DISTINCT questions.id) = COUNT(user_responses.id)
-#
+#   COUNT(DISTINCT questions.id) = COUNT(responses.id)
+#   #
+
+
 # SELECT
 #   *
 # FROM
