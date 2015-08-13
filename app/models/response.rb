c5fc2112adb99910e069fee78ddcc85e93bcd3a9
= <=>
2015-08-13 11:32:08 -0400

    implement not_already_answered validation

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,7 @@ class Response < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :answer_choice_id, :presence => true
+  validate :respondent_has_not_already_answered_question
 
   belongs_to :respondent,
     class_name: "User",
@@ -18,8 +19,21 @@ class Response < ActiveRecord::Base
     source: :question
 
   def sibling_responses
-    question.responses.where("responses.id != ?", id)
+    if id.nil?
+      question.responses
+    else
+      question.responses.where("responses.id != ?", id)
+    end
   end
 
+  def respondent_has_not_already_answered_question
+    if sibling_responses.any? { |response| response.user_id == respondent.id }
+      errors[:already_answered] << "user has already answered this question"
+    end
+  end
+
+  # def respondent_is_not_poll_author
+  #   answer_choice
+  # end
 
 end
